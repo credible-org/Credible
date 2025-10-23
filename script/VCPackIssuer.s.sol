cb6902b33ac378841663209885cdb657700c7bfc
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2025-10-23 17:16:15 +0200

    update contract

--- script/VCPackIssuer.s.sol
+++ script/VCPackIssuer.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {console} from "../lib/forge-std/src/console.sol";
 
 import {VCPackIssuer} from "../src/VCPackIssuer.sol"; 
 
