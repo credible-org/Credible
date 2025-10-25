ccba2144c6184aa7c36c7f8a261237cdcd10e624
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2025-10-25 20:12:14 +0200

    Completed Smart Contract

--- src/VCPackIssuer.sol
+++ src/VCPackIssuer.sol
@@ -6,7 +6,6 @@ import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.so
 import {Context} from "../lib/openzeppelin-contracts/contracts/utils/Context.sol";
 import {Strings} from "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
-// FIX: Removed explicit inheritance of 'Context' as it is inherited by ERC1155.
 contract VCPackIssuer is ERC1155, Ownable {
     using Strings for uint256;
 
@@ -17,13 +16,16 @@ contract VCPackIssuer is ERC1155, Ownable {
     }
 
     struct Milestone {
+        string id;
+        string title;
         string description;
+        string required;
     }
 
     struct Pack {
         uint256 id;
         string name;
-        string metadataURI;
+        string description;
         Milestone[] milestones;
     }
 
@@ -35,45 +37,43 @@ contract VCPackIssuer is ERC1155, Ownable {
 
     mapping(address => mapping(uint256 => mapping(uint256 => uint8))) public submissionStatus;
 
-    event PackCreated(uint256 indexed packId, string name, string metadataURI, uint256 milestoneCount);
+    event PackCreated(uint256 indexed packId, string name, string description, uint256 milestoneCount);
     event MilestoneSubmitted(address indexed holder, uint256 indexed packId, uint256 milestoneIndex, string proofCID);
-    event MilestoneApproved(address indexed holder, uint256 indexed packId, uint256 milestoneIndex, uint256 newProgress);
-    event MilestoneRejected(address indexed holder, uint256 indexed packId, uint256 milestoneIndex);
-    event PackMinted(address indexed holder, uint256 indexed packId, string finalMetadataURI);
+    event MilestoneApproved(address indexed holder, uint256 indexed packId,  uint256 milestoneIndex, string feedback, uint256 newProgress);
+    event MilestoneRejected(address indexed holder, uint256 indexed packId, string feedback, uint256 milestoneIndex);
+    event PackMinted(address indexed holder, uint256 indexed packId);
 
     constructor(string memory _uri, address initialOwner) ERC1155(_uri) Ownable(initialOwner) {}
 
     function createPack(
         string memory _name,
-        string memory _metadataURI,
+        string memory _description,
         Milestone[] memory _milestones
     ) external onlyOwner {
         require(_milestones.length > 0, "Pack must have at least one milestone");
 
         uint256 currentPackId = nextPackId;
 
-        // FIX: Instead of copying the dynamic array directly (which fails in legacy pipeline),
-        // we use a storage reference and push elements iteratively.
+        
         Pack storage newPack = packs[currentPackId];
 
-        // Initialize scalar/static members
         newPack.id = currentPackId;
         newPack.name = _name;
-        newPack.metadataURI = _metadataURI;
-
-        // Copy dynamic array (milestones) element by element
+        newPack.description = _description;
+        
         for (uint256 i = 0; i < _milestones.length; i++) {
             newPack.milestones.push(_milestones[i]);
         }
 
         nextPackId++;
 
-        emit PackCreated(currentPackId, _name, _metadataURI, _milestones.length);
+        emit PackCreated(currentPackId, _name, _description, _milestones.length);
     }
 
     function approveMilestone(
         address _holder,
         uint256 _packId,
+        string memory feedback,
         uint256 _milestoneIndex
     ) external onlyOwner {
         Pack storage pack = packs[_packId];
@@ -87,12 +87,13 @@ contract VCPackIssuer is ERC1155, Ownable {
 
         holderProgress[_holder][_packId]++;
 
-        emit MilestoneApproved(_holder, _packId, _milestoneIndex, holderProgress[_holder][_packId]);
+        emit MilestoneApproved(_holder, _packId, _milestoneIndex, feedback, holderProgress[_holder][_packId]);
     }
 
     function rejectMilestone(
         address _holder,
         uint256 _packId,
+        string memory feedback,
         uint256 _milestoneIndex
     ) external onlyOwner {
         Pack storage pack = packs[_packId];
@@ -104,7 +105,7 @@ contract VCPackIssuer is ERC1155, Ownable {
 
         submissionStatus[_holder][_packId][_milestoneIndex] = uint8(SubmissionStatus.NotSubmitted);
 
-        emit MilestoneRejected(_holder, _packId, _milestoneIndex);
+        emit MilestoneRejected(_holder, _packId, feedback, _milestoneIndex);
     }
 
     function submitMilestone(
@@ -113,7 +114,7 @@ contract VCPackIssuer is ERC1155, Ownable {
         string memory _proofCID
     ) external {
         Pack storage pack = packs[_packId];
-        require(pack.id != 0, "Pack does not exist");
+        require(pack.id != 0, "Pack does not exist"); //check
         require(_milestoneIndex < pack.milestones.length, "Invalid milestone index");
 
         uint8 currentStatus = submissionStatus[_msgSender()][_packId][_milestoneIndex];
@@ -129,8 +130,7 @@ contract VCPackIssuer is ERC1155, Ownable {
     }
 
     function mintCompletedPack(
-        uint256 _packId,
-        string memory _finalMetadataURI
+        uint256 _packId
     ) external {
         Pack storage pack = packs[_packId];
         require(pack.id != 0, "Pack does not exist");
@@ -144,10 +144,12 @@ contract VCPackIssuer is ERC1155, Ownable {
 
         _mint(_msgSender(), _packId, 1, "");
 
-        emit PackMinted(_msgSender(), _packId, _finalMetadataURI);
+        emit PackMinted(_msgSender(), _packId);
     }
 
     function getRequiredMilestoneCount(uint256 _packId) public view returns (uint256) {
         return packs[_packId].milestones.length;
     }
 }
+
+
